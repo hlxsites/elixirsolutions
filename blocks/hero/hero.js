e3ce530cc9aaf81d8a89d0e4779d3c0bef5397f2
Dragos Dascalita Haut <ddragosd@gmail.com>
2023-05-10 22:37:05 -1000

    PR review updates

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
   const elementContainer = block.querySelector(':scope > div > div');
   const rightBox = document.createElement('div');
-  rightBox.classList.add('right-box');
+  rightBox.classList.add('hero-image-container');
   rightBox.append(elementContainer.querySelector('picture'));
   block.append(rightBox);
 
   const leftBox = document.createElement('div');
-  leftBox.classList.add('left-box');
+  leftBox.classList.add('hero-content-container');
   leftBox.append(...elementContainer.children);
   block.prepend(leftBox);
   elementContainer.parentElement.remove();
@@ -19,9 +19,9 @@ export default function decorate(block) {
   // }
   // add two divs for two background circles
   const c1 = document.createElement('div');
-  c1.classList.add('bg-circle-1');
+  c1.classList.add('bg-circle-1', 'bg-circle');
   leftBox.append(c1);
   const c2 = document.createElement('div');
-  c2.classList.add('bg-circle-2');
+  c2.classList.add('bg-circle-2', 'bg-circle');
   leftBox.append(c2);
 }
