e6034bfe6dcc06158e2d6ceb9f1f4e16fb462dea
Sean Steimer <ssteimer@adobe.com>
2023-05-11 11:40:36 -0700

    refactor hero circles to use pseudo elems and add alternato color for hero

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -10,18 +10,4 @@ export default function decorate(block) {
   leftBox.append(...elementContainer.children);
   block.prepend(leftBox);
   elementContainer.parentElement.remove();
-
-  // leftBox.append(h1);
-  // if (h2 !== null) leftBox.append(h2);
-  // if (cta !== null) {
-  //   leftBox.append(cta);
-  //   cta.classList.add('button');
-  // }
-  // add two divs for two background circles
-  const c1 = document.createElement('div');
-  c1.classList.add('bg-circle-1', 'bg-circle');
-  leftBox.append(c1);
-  const c2 = document.createElement('div');
-  c2.classList.add('bg-circle-2', 'bg-circle');
-  leftBox.append(c2);
 }
