ea931252070c4ec42391d8cc1811e273f03fbf7f
Dragos Dascalita Haut <ddragosd@gmail.com>
2023-05-19 10:42:05 -1000

    PR review and dropdown arrow

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -7,7 +7,10 @@ export function addDropdown(contentBox) {
   // by convention the title is the first item in the unordered list
   const dropdownTitle = dropDownSourceElement.textContent.split('\n')[0];
 
+  const selectElWrapper = document.createElement('div');
+  selectElWrapper.classList.add('hero-select-wrapper');
   const selectEl = document.createElement('select');
+
   const ul = contentBox.querySelector('ul > li > ul');
   ul.querySelectorAll('li').forEach((liElement) => {
     const t = liElement.querySelector('a');
@@ -25,7 +28,8 @@ export function addDropdown(contentBox) {
   const btnContainer = contentBox.querySelector('.button-container');
   const btn = contentBox.querySelector('.button-container > a');
   btn.href = selectEl.options[selectEl.selectedIndex].value;
-  contentBox.insertBefore(selectEl, btnContainer);
+  selectElWrapper.appendChild(selectEl);
+  contentBox.insertBefore(selectElWrapper, btnContainer);
 
   // add an event listener to the dropdown to redirect to the selected link
   selectEl.addEventListener('change', () => {
@@ -36,7 +40,7 @@ export function addDropdown(contentBox) {
   // insert the title of the dropdown
   const titleEl = document.createElement('h5');
   titleEl.textContent = dropdownTitle;
-  contentBox.insertBefore(titleEl, selectEl);
+  contentBox.insertBefore(titleEl, selectElWrapper);
 }
 
 export function removeEmptyPTags(element) {
