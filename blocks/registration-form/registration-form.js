573165deefcf47c137e981b116005321e6a91547
Dragos Dascalita Haut <ddragosd@gmail.com>
2023-05-25 15:03:36 -1000

    step 1 or the form

--- blocks/registration-form/registration-form.js
+++ blocks/registration-form/registration-form.js
@@ -1,46 +1,77 @@
 import ffetch from '../../scripts/ffetch.js';
+import { createElement } from '../../scripts/scripts.js';
 
-let rxbinData = [];
+const RXBIN_REGISTRY = '/register-login/register-login.json';
+//   const GROUP_REGISTRY = '/register-login.json?sheet=groups';
 
 function buildStep1(rows) {
   rows[1].remove();
   rows[2].remove();
   const cols = [...rows[0].children];
   // 0. step1 container
-  const step1Container = document.createElement('div');
-  step1Container.classList.add('registration-form-step');
+  const step1Container = createElement('div', 'registration-form-step');
   // 1. build the form
-  const formContainer = document.createElement('div');
-  formContainer.classList.add('form-container');
+  const formContainer = createElement('div', 'form-container');
   formContainer.append(cols[0]);
   step1Container.append(formContainer);
   // add form input
-  const textBox = document.createElement('input');
-  textBox.required = true;
-  textBox.placeholder = 'e.g. 012718';
+  const textBox = createElement('input', 'registration-form-input', {
+    required: true,
+    placeholder: 'e.g. 012718',
+  });
   formContainer.append(textBox);
-  // TODO: on key up filter by rxbin
-  console.log(`RxBinData: ${rxbinData}`);
+
+  const textBoxTooltipContainers = createElement('div');
+  const textBoxTooltip = createElement('span', 'registration-form-input-tooltip');
+  textBoxTooltip.innerText = 'TEXXTTTT';
+  formContainer.append(textBoxTooltipContainers);
+  textBoxTooltipContainers.append(textBoxTooltip);
 
   // add button
-  const searchBtn = document.createElement('button');
-  searchBtn.type = 'submit';
+  const searchBtn = createElement('button', 'button', {
+    type: 'submit',
+  });
   searchBtn.innerText = 'Search';
-  searchBtn.classList.add('button');
   formContainer.append(searchBtn);
 
+  searchBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const rxInput = textBox.value.toLowerCase();
+    // {
+    //     Rx Bin: "3585",
+    //     Requires Group Lookup: "No",
+    //     URL: "https://member.envisionpharmacies.com/PortalUser/EpharmPortalSignin"
+    // }
+    const rxItems = await ffetch(RXBIN_REGISTRY).all();
+    const rxItem = rxItems.filter((rx) => rx['Rx Bin'] === rxInput).pop();
+    if (rxItem === null) {
+      textBoxTooltip.innerText = 'Please provide a valid Rx Bin Number.';
+      textBoxTooltip.style.visibility = 'visible';
+      textBoxTooltip.style.opacity = 1;
+      // Hide the tooltip after 5 seconds
+      setTimeout(() => {
+        textBoxTooltip.style.opacity = 0;
+      }, 5000);
+      return;
+    }
+    // rxItem is found
+    if (rxItem['Requires Group Lookup'] === 'No') {
+      const url = rxItem.URL;
+      // follow the URL
+      window.open(url, '_blank');
+    }
+    // TODO: move to step 2 for the form
+  });
+
   // 2. show the image on the right
-  const imageContainer = document.createElement('div');
-  imageContainer.classList.add('image-container');
+  const imageContainer = createElement('div', 'image-container');
   imageContainer.append(cols[1]);
   step1Container.append(imageContainer);
 
   return step1Container;
 }
 
-export default async function decorate(block) {
-  const RXBIN_REGISTRY = '/register-login/register-login.json';
-  //   const GROUP_REGISTRY = '/register-login.json?sheet=groups';
+export default function decorate(block) {
   // expect three rows
   const rows = [...block.children];
   if (rows.length < 3) {
@@ -49,5 +80,4 @@ export default async function decorate(block) {
   }
   // TODO: depending on query params, set the correct step
   block.append(buildStep1(rows));
-  rxbinData = await ffetch(RXBIN_REGISTRY).all();
 }
