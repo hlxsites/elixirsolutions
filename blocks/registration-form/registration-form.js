09b5da98f3a960cb095c2f8dbe3396a62f46d7f2
Sean Steimer <ssteimer@adobe.com>
2023-05-31 15:36:23 -0700

    refactoring

--- blocks/registration-form/registration-form.js
+++ blocks/registration-form/registration-form.js
@@ -3,144 +3,101 @@ import { createElement } from '../../scripts/scripts.js';
 
 // the name of the sheet where the groups are defined
 const GROUP_REGISTRY_SHEET_NAME = 'groups';
-// in-memory cache for the group lookup, populated during buildStep2()
+// in-memory cache for the group lookup
+// populated when step 2 is displayed
 let groupsRegistryCache;
 
-function buildStep2(rows, block, groupRegistry) {
-  block.append(rows[1]);
-  const cols = [...rows[1].children];
+function buildForm(serchFn) {
+  const wrapper = createElement('div', 'form-wrapper');
 
-  // 0. step1 container
-  const stepContainer = createElement('div', 'registration-form-step');
-  // 1. build the form
-  const formContainer = createElement('div', 'form-container');
-  formContainer.append(cols[0]);
-  stepContainer.append(formContainer);
-  // add form input
-  const textBox = createElement('input', 'registration-form-input', {
+  wrapper.append(createElement('input', 'registration-form-input', {
     required: true,
     placeholder: 'e.g. 012718',
-  });
-  formContainer.append(textBox);
+  }));
 
   const textBoxTooltipContainers = createElement('div');
   const textBoxTooltip = createElement('span', 'registration-form-input-tooltip');
   textBoxTooltip.innerText = '';
-  formContainer.append(textBoxTooltipContainers);
+  wrapper.append(textBoxTooltipContainers);
   textBoxTooltipContainers.append(textBoxTooltip);
 
-  // add button
   const searchBtn = createElement('button', 'button', {
     type: 'submit',
-  });
-  searchBtn.innerText = 'Search';
-  formContainer.append(searchBtn);
+  }, 'Search');
+  searchBtn.addEventListener('click', serchFn);
+  wrapper.append(searchBtn);
 
-  searchBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const grpInput = textBox.value.toLowerCase();
-
-    groupsRegistryCache = (groupsRegistryCache
-                          || await ffetch(groupRegistry)
-                            .sheet(GROUP_REGISTRY_SHEET_NAME)
-                            .chunks(1000)
-                            .all());
-    const grpItems = groupsRegistryCache;
-    // {
-    //   Group Id: "106110T80902",
-    //   URL: "https://www.elixirsolutions.com/new-plans"
-    // }
-    const grpItem = grpItems.filter((rx) => rx['Group Id'].toLowerCase() === grpInput).pop();
-    if (grpItem === null || typeof (grpItem) === 'undefined') {
-      textBoxTooltip.innerText = 'Please provide a valid Group Number.';
-      textBoxTooltip.style.visibility = 'visible';
-      textBoxTooltip.style.opacity = 1;
-      // Hide the tooltip after 5 seconds
-      setTimeout(() => {
-        textBoxTooltip.style.opacity = 0;
-      }, 5000);
-      return;
-    }
-    // grpItem was found
-    const url = grpItem.URL;
-    // follow the URL
-    window.open(url, '_blank');
-  });
-
-  // 2. show the image on the right
-  const imageContainer = createElement('div', 'image-container');
-  imageContainer.append(cols[1]);
-  stepContainer.append(imageContainer);
-
-  block.append(stepContainer);
+  return wrapper;
 }
 
-function buildStep1(rows, block, rxBinRegistry) {
-  rows[1].remove();
-  rows[2].remove();
-  const cols = [...rows[0].children];
-  // 0. step1 container
-  const step1Container = createElement('div', 'registration-form-step');
-  // 1. build the form
-  const formContainer = createElement('div', 'form-container');
-  formContainer.append(cols[0]);
-  step1Container.append(formContainer);
-  // add form input
-  const textBox = createElement('input', 'registration-form-input', {
-    required: true,
-    placeholder: 'e.g. 012718',
-  });
-  formContainer.append(textBox);
+function decorateFormSteps(row, i, serchFn) {
+  row.classList.add('registration-form-step', `registration-form-step-${i}`);
+  const formContainer = row.children[0];
+  formContainer.classList.add('form-container');
+  const form = buildForm(serchFn);
+  formContainer.append(form);
 
-  const textBoxTooltipContainers = createElement('div');
-  const textBoxTooltip = createElement('span', 'registration-form-input-tooltip');
-  textBoxTooltip.innerText = '';
-  formContainer.append(textBoxTooltipContainers);
-  textBoxTooltipContainers.append(textBoxTooltip);
+  const imageContainer = row.children[1];
+  imageContainer.classList.add('image-container');
+}
 
-  // add button
-  const searchBtn = createElement('button', 'button', {
-    type: 'submit',
-  });
-  searchBtn.innerText = 'Search';
-  formContainer.append(searchBtn);
+async function stepOneLookup(lookupUrl, lookupValue, textBoxTooltip, block) {
+  // {
+  //     Rx Bin: "3585",
+  //     Requires Group Lookup: "No",
+  //     URL: "https://member.envisionpharmacies.com/PortalUser/EpharmPortalSignin"
+  // }
+  const rxItem = await ffetch(lookupUrl)
+    .filter((rx) => rx['Rx Bin'] === lookupValue)
+    .first();
+  // const rxItem = rxItems.filter((rx) => rx['Rx Bin'] === rxInput).pop();
+  if (rxItem === null || typeof (rxItem) === 'undefined') {
+    textBoxTooltip.innerText = 'Please provide a valid Rx Bin Number.';
+    textBoxTooltip.style.visibility = 'visible';
+    textBoxTooltip.style.opacity = 1;
+    // Hide the tooltip after 5 seconds
+    setTimeout(() => {
+      textBoxTooltip.style.opacity = 0;
+    }, 5000);
+    return;
+  }
+  // rxItem is found
+  if (rxItem['Requires Group Lookup'] === 'No') {
+    const url = rxItem.URL;
+    // follow the URL
+    window.open(url, '_blank');
+    return;
+  }
 
-  searchBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const rxInput = textBox.value.toLowerCase();
-    // {
-    //     Rx Bin: "3585",
-    //     Requires Group Lookup: "No",
-    //     URL: "https://member.envisionpharmacies.com/PortalUser/EpharmPortalSignin"
-    // }
-    const rxItems = await ffetch(rxBinRegistry).all();
-    const rxItem = rxItems.filter((rx) => rx['Rx Bin'] === rxInput).pop();
-    if (rxItem === null || typeof (rxItem) === 'undefined') {
-      textBoxTooltip.innerText = 'Please provide a valid Rx Bin Number.';
-      textBoxTooltip.style.visibility = 'visible';
-      textBoxTooltip.style.opacity = 1;
-      // Hide the tooltip after 5 seconds
-      setTimeout(() => {
-        textBoxTooltip.style.opacity = 0;
-      }, 5000);
-      return;
-    }
-    // rxItem is found
-    if (rxItem['Requires Group Lookup'] === 'No') {
-      const url = rxItem.URL;
-      // follow the URL
-      window.open(url, '_blank');
-    }
-    step1Container.remove();
-    buildStep2(rows, block, rxBinRegistry);
-  });
+  // load the cache for next step
+  groupsRegistryCache = ffetch(lookupUrl)
+    .sheet(GROUP_REGISTRY_SHEET_NAME)
+    .chunks(1000)
+    .all();
 
-  // 2. show the image on the right
-  const imageContainer = createElement('div', 'image-container');
-  imageContainer.append(cols[1]);
-  step1Container.append(imageContainer);
+  block.classList.add('step-2');
+  block.classList.remove('step-1');
+}
 
-  block.append(step1Container);
+async function stepTwoLookup(lookupValue, textBoxTooltip) {
+  const groupData = await groupsRegistryCache;
+  const grpItem = groupData
+    .find((rx) => rx['Group Id'].toLowerCase() === lookupValue);
+
+  if (grpItem === null || typeof (grpItem) === 'undefined') {
+    textBoxTooltip.innerText = 'Please provide a valid Group Number.';
+    textBoxTooltip.style.visibility = 'visible';
+    textBoxTooltip.style.opacity = 1;
+    // Hide the tooltip after 5 seconds
+    setTimeout(() => {
+      textBoxTooltip.style.opacity = 0;
+    }, 5000);
+    return;
+  }
+  // grpItem was found
+  const url = grpItem.URL;
+  // follow the URL
+  window.open(url, '_blank');
 }
 
 export default function decorate(block) {
@@ -155,5 +112,23 @@ export default function decorate(block) {
   if (rxBinRegistry === null || typeof (rxBinRegistry) === 'undefined') {
     return;
   }
-  buildStep1(rows, block, rxBinRegistry);
+  rows[2].remove();
+
+  decorateFormSteps(rows[0], 1, async (evt) => {
+    evt.preventDefault();
+    const formWrapper = evt.currentTarget.closest('.form-wrapper');
+    const textBox = formWrapper.querySelector('.registration-form-input');
+    const rxInput = textBox.value.toLowerCase();
+    const textBoxTooltip = formWrapper.querySelector('.registration-form-input-tooltip');
+    stepOneLookup(rxBinRegistry, rxInput, textBoxTooltip, block);
+  });
+  decorateFormSteps(rows[1], 2, async (evt) => {
+    evt.preventDefault();
+    const formWrapper = evt.currentTarget.closest('.form-wrapper');
+    const textBox = formWrapper.querySelector('.registration-form-input');
+    const grpInput = textBox.value.toLowerCase();
+    const textBoxTooltip = formWrapper.querySelector('.registration-form-input-tooltip');
+    stepTwoLookup(grpInput, textBoxTooltip);
+  });
+  block.classList.add('step-1');
 }
